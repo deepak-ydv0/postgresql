89641a1e652b2d94021aa81d3ec449fd4b29628b
DPK-YDV <kd806040@gmail.com>
2025-04-04 22:45:35 +0530

    Send status and json.

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,5 +1,9 @@
 const registerControllers = async (req, res) => {
   console.log("user register");
+  res.status(200).json({
+    success: true,
+    message: "test checked",
+  });
 };
 
 export { registerControllers };
